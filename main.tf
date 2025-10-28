b04ba75f2fcc79f2c4ce683195e68f666613c517
Ivan <nikitsenka@gmail.com>
2025-10-28 14:47:21 +0100

    add Amazon OpenSearch Collection

--- main.tf
+++ main.tf
@@ -35,3 +35,81 @@ data "aws_caller_identity" "current" {}
 resource "aws_s3_bucket" "bedrock_kb_bucket" {
   bucket = local.s3_bucket_name
 }
+
+resource "aws_opensearchserverless_security_policy" "encryption_policy" {
+  name = "bedrock-kb-${local.resource_suffix}-enc"
+  type = "encryption"
+  policy = jsonencode({
+    Rules = [
+      {
+        Resource     = ["collection/${local.aoss_collection_name}"]
+        ResourceType = "collection"
+      }
+    ]
+    AWSOwnedKey = true
+  })
+}
+
+resource "aws_opensearchserverless_security_policy" "network_policy" {
+  name = "bedrock-kb-${local.resource_suffix}-net"
+  type = "network"
+  policy = jsonencode([
+    {
+      Rules = [
+        {
+          Resource     = ["collection/${local.aoss_collection_name}"]
+          ResourceType = "collection"
+        }
+      ]
+      AllowFromPublic = true
+    }
+  ])
+}
+
+resource "aws_opensearchserverless_access_policy" "data_policy" {
+  name = "bedrock-kb-${local.resource_suffix}-data"
+  type = "data"
+  policy = jsonencode([
+    {
+      Rules = [
+        {
+          Resource     = ["collection/${local.aoss_collection_name}"]
+          ResourceType = "collection"
+          Permission = [
+            "aoss:CreateCollectionItems",
+            "aoss:DeleteCollectionItems",
+            "aoss:UpdateCollectionItems",
+            "aoss:DescribeCollectionItems"
+          ]
+        },
+        {
+          Resource     = ["index/${local.aoss_collection_name}/*"]
+          ResourceType = "index"
+          Permission = [
+            "aoss:CreateIndex",
+            "aoss:DeleteIndex",
+            "aoss:UpdateIndex",
+            "aoss:DescribeIndex",
+            "aoss:ReadDocument",
+            "aoss:WriteDocument"
+          ]
+        }
+      ]
+      Principal = [
+        data.aws_caller_identity.current.arn
+      ]
+      Description = "Data access policy for Bedrock Knowledge Base"
+    }
+  ])
+}
+
+resource "aws_opensearchserverless_collection" "bedrock_kb_collection" {
+  name = local.aoss_collection_name
+  type = "VECTORSEARCH"
+
+  depends_on = [
+    aws_opensearchserverless_security_policy.encryption_policy,
+    aws_opensearchserverless_security_policy.network_policy,
+    aws_opensearchserverless_access_policy.data_policy
+  ]
+}
